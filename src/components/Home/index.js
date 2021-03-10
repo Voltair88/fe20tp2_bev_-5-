a6b6d6440a06afa2b24f25073cda50a3ab39ef44
Tanngrisnr <fredrik.wintzell@gmail.com>
2021-03-10 13:04:16 +0100

    removed message api from homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,8 @@ const HomePage = () => {
     <div>
       <h1>Home</h1>
       <p>The Home Page is accessible by every signed in user.</p>
-      <Messages />
+
+      {/* <Messages /> */}
     </div>
   );
 };
