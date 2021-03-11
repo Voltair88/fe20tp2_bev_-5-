70975730e83cadbb83eed866a59ec56f1e325d96
Tanngrisnr <fredrik.wintzell@gmail.com>
2021-03-11 11:47:44 +0100

    changed to switch routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { withAuthentication } from "../Session";
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -16,13 +16,37 @@ const App = () => (
   <Router>
     <Navigation />
     <hr />
-    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+
+    <Switch>
+      <Route exact path={ROUTES.LANDING}>
+        <LandingPage />
+      </Route>
+      <Route path={ROUTES.SIGN_UP}>
+        <SignUpPage />
+      </Route>
+      <Route path={ROUTES.SIGN_IN}>
+        <SignInPage />
+      </Route>
+      <Route path={ROUTES.PASSWORD_FORGET}>
+        <PasswordForgetPage />
+      </Route>
+      <Route path={ROUTES.HOME}>
+        <HomePage />
+      </Route>
+      <Route path={ROUTES.ACCOUNT}>
+        <AccountPage />
+      </Route>
+      <Route path={ROUTES.ADMIN}>
+        <AdminPage />
+      </Route>
+    </Switch>
+    {/*     <Route exact path={ROUTES.LANDING} component={LandingPage} />
     <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
     <Route path={ROUTES.SIGN_IN} component={SignInPage} />
     <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
     <Route path={ROUTES.HOME} component={HomePage} />
     <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-    <Route path={ROUTES.ADMIN} component={AdminPage} />
+    <Route path={ROUTES.ADMIN} component={AdminPage} /> */}
   </Router>
 );
 
