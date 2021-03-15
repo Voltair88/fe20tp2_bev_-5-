fac005b2f8770515986d5c9a48830a0ae64ed1e8
Egeeoz <70132005+Egeeoz@users.noreply.github.com>
2021-03-15 14:26:40 +0100

    Made seperate file for styled components and moved them there

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,68 +1,11 @@
-import { Link } from "react-router-dom";
 import { AuthUserContext } from "../Session";
 import * as ROUTES from "../../constants/routes";
 import * as ROLES from "../../constants/roles";
 import SignOutButton from "../SignOut";
 
-import styled from "styled-components";
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-
-const Navbar = styled.div`
-  width: 100%;
-  height: 8vh;
-`;
-
-const Ul = styled.ul`
-  margin: 0;
-  padding-right: 2.5rem;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  list-style: none;
-  background: rgb(32, 224, 63);
-  background: linear-gradient(
-    0deg,
-    rgba(32, 224, 63, 1) 0%,
-    rgba(31, 119, 22, 1) 100%
-  );
-  height: 100%;
-  font-size: 1.2rem;
-  & > li {
-    padding: 1rem;
-
-    @media screen and (max-width: 768px) {
-      display: none;
-    }
-  }
-`;
-
-const NavLink = styled(Link)`
-  text-decoration: none;
-  text-transform: uppercase;
-  color: white;
-  font-weight: 500;
-  font-family: "Poppins", sans-serif;
-
-  &:hover {
-    color: black;
-    transition: 0.3s ease-in-out;
-  }
-`;
-
-const HamMenu = styled(FontAwesomeIcon)`
-  color: white;
-  font-size: 1.6rem;
-  margin-right: auto;
-  cursor: pointer;
-  display: none;
-
-  @media screen and (max-width: 768px) {
-    display: block;
-    margin-left: -1rem;
-  }
-`;
+import { Navbar, Ul, NavLink, HamMenu } from "../StyledCom";
 
 const Navigation = () => {
   return (
@@ -80,31 +23,41 @@ const Navigation = () => {
   );
 };
 
-const NavigationAuth = ({ authUser }) => (
-  <Ul>
-    <HamMenu icon={faBars} />
-    <li>
-      <NavLink to={ROUTES.LANDING}>Landing</NavLink>
-    </li>
-    <li>
-      <NavLink to={ROUTES.HOME}>Home</NavLink>
-    </li>
-    <li>
-      <NavLink to={ROUTES.ACCOUNT}>Account</NavLink>
-    </li>
-    {!!authUser.roles[ROLES.ADMIN] && (
+const MenuToggle = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  if (showMenu) {
+  }
+
+  return <HamMenu icon={faBars} onClick={() => setShowMenu(!showMenu)} />;
+};
+
+const NavigationAuth = ({ authUser }) => {
+  return (
+    <Ul>
+      <MenuToggle />
       <li>
-        <NavLink to={ROUTES.ADMIN}>Admin</NavLink>
+        <NavLink to={ROUTES.LANDING}>Landing</NavLink>
       </li>
-    )}
-    <li>
-      <SignOutButton />
-    </li>
-  </Ul>
-);
+      <li>
+        <NavLink to={ROUTES.HOME}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={ROUTES.ACCOUNT}>Account</NavLink>
+      </li>
+      {!!authUser.roles[ROLES.ADMIN] && (
+        <li>
+          <NavLink to={ROUTES.ADMIN}>Admin</NavLink>
+        </li>
+      )}
+      <li>
+        <SignOutButton />
+      </li>
+    </Ul>
+  );
+};
 const NavigationNonAuth = () => (
   <Ul>
-    <HamMenu icon={faBars} />
+    <MenuToggle />
     <li>
       <NavLink to={ROUTES.LANDING}>Landing</NavLink>
     </li>
