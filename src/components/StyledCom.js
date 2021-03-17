13cc9928ea87f50b4b727d83c55e111a563edc79
Ivar <ivarmun@gmail.com>
2021-03-17 13:28:01 +0100

    fixed all loose styled components

--- src/components/StyledCom.js
+++ src/components/StyledCom.js
@@ -178,4 +178,23 @@ export const ChartContainer = styled.div`
   position: absolute;
   left: 10px;
   top: 40px;
-`
+`;
+
+export const outButton = styled.button`
+font-weight: 500;
+font-family: "Poppins", sans-serif;
+text-transform: uppercase;
+color: white;
+padding: 0.3rem 0.6rem;
+font-size: 1.1rem;
+border: 2px solid white;
+background: none;
+border-radius: 6px;
+cursor: pointer;
+
+&:hover {
+  border-color: black;
+  color: black;
+  transition: 0.3s ease-in-out;
+}
+`;
