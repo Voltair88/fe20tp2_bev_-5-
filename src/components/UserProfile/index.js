6a162c51b9bd878610ccbda8d25118c60e3963df
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-04-15 11:58:58 +0200

    Removed competition from userprofile,changed the Dropdown comp

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -2,19 +2,10 @@ import React, { useState, useEffect } from "react";
 import Dropdown from "../Dropdown";
 import { AuthUserContext, withAuthentication } from "../Session";
 import ProfileImage from "../../img/prf_img.png";
-import { CL_TEAMS_DATA } from "../../data.js";
-import { TEAM_DATA } from "../../data.js";
+import { requestOptions } from "../../data.js";
 import styled from "styled-components";
-import PasswordChangeForm from "../PasswordChange";
-import Pencil from "../../img/pencil.png"
 import SnackbarComponent from "../SnackbarComponent";
-import EditIcon from '@material-ui/icons/Edit';
 import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import PlayerChart from "../PlayerChart";
-
-
 
 
 
@@ -68,18 +59,13 @@ function UserProfile(props) {
 
     const user = React.useContext(AuthUserContext);
 
-    const [image, setImage] = useState(null);
     const [uid, setUid] = useState(null);
     const [url, setUrl] = useState(ProfileImage);
 
-    const [fav_competition, setFav_competition] = useState(null);
+    /* const [fav_competition, setFav_competition] = useState(null); */
     const [fav_player, setFav_player] = useState(null);
     const [fav_team, setFav_team] = useState(null);
-
-    /* const [player_array, setPlayer_array] = useState([]);
-    const [team_array, setTeam_array] = useState([]); */
-
-    const [userName, setUserName] = useState();
+    const [userName, setUserName] = useState('');
 
 
 
@@ -88,18 +74,9 @@ function UserProfile(props) {
     const [message, setMessage] = useState('');
     const [severity, setSeverity] = useState('');
 
-    const [competitionsArr, setCompetitionsArr] = useState([]);
-    const [competitionsLoaded, setCompetitionsLoaded] = useState(false);
 
     const [teamsArr, setTeamsArr] = useState([]);
-    const [teamsLoaded, setTeamsLoaded] = useState(false);
-
     const [squadArr, setSquadArr] = useState([]);
-    const [squadLoaded, setSquadLoaded] = useState(false);
-
-
-
-    const [selectedCompetitionId, setSelectedCompetitionId] = useState();
     const [selectedTeamId, setSelectedTeamId] = useState();
 
     const handleSnackbar = (message, severity) => {
@@ -114,10 +91,7 @@ function UserProfile(props) {
 
     const eventhandler = (selectedOption, check) => {
         if (!!selectedOption) {
-            if (check === 'COMPETITION') {
-                setSelectedCompetitionId(selectedOption.value) //Set selected competition id to use when fetch favorite team
-            }
-            else if (check === 'TEAMS') {
+            if (check === 'TEAMS') {
                 setSelectedTeamId(selectedOption.value) //Set selected competition id to use when fetch favorite team
             }
         }
@@ -125,85 +99,17 @@ function UserProfile(props) {
     }
 
 
-
     useEffect(() => {
+
+
         setUid(user.uid);
         bindData(user.uid);
         setUserName(user.username);
 
-        //Read CL_TEAMS_DATA from data.js and choose only id and team name from that data and store in an array
-        /* let teamArr = [];
-        for (let key of Object.keys(CL_TEAMS_DATA.teams)) {
-            teamArr.push({
-                value: CL_TEAMS_DATA.teams[key].id,
-                label: CL_TEAMS_DATA.teams[key].name,
-            });
-        }
-        setTeam_array(teamArr); */
-        //End CL_TEAMS_DATA team
+        //1. Fetch teams according to league that a specific user has on firebase db
+        if (!!user.league) {
 
-
-        /* let player_array = [];
-        for (let key of Object.keys(TEAM_DATA.squad)) {
-            player_array.push({
-                value: TEAM_DATA.squad[key].id,
-                label: TEAM_DATA.squad[key].name,
-            });
-        }
-        setPlayer_array(player_array); */
-
-
-        //Fetch data from API
-        //1. Fetch competitions data
-        if (!competitionsLoaded) {
-            var myHeaders = new Headers();
-            myHeaders.append("X-Auth-Token", "ef2c88344214470198023fffda588093");
-
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
-
-            fetch("http://api.football-data.org/v2/competitions/", requestOptions)
-                .then(response => response.json())
-                .then((data) => {
-                    if (data !== undefined) {
-
-
-                        let competitions = [];
-                        for (let key of Object.keys(data.competitions)) {
-                            competitions.push({
-                                value: data.competitions[key].id,
-                                label: data.competitions[key].name,
-                            });
-                        }
-                        setCompetitionsArr(competitions)
-                        setCompetitionsLoaded(true) //Set the "competitions loaded" to true
-                    } else {
-                        handleSnackbar("Couldn't found the competition", "error");
-                    }
-
-                })
-                .catch(err => {
-                    //console.error(err);
-                    handleSnackbar("Couldn't found the competition", "error");
-                });
-        }
-
-
-        //2. Fetch teams data if user has already selected a competition
-        if (selectedCompetitionId !== undefined) {
-            var myHeaders = new Headers();
-            myHeaders.append("X-Auth-Token", "ef2c88344214470198023fffda588093");
-
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
-
-            fetch(`http://api.football-data.org/v2/competitions/${selectedCompetitionId}/teams`, requestOptions)
+            fetch(`http://api.football-data.org/v2/competitions/${user.league}/teams`, requestOptions)
                 .then(response => response.json())
                 .then((data) => {
                     if (data !== undefined) {
@@ -215,33 +121,20 @@ function UserProfile(props) {
                             });
                         }
                         setTeamsArr(teams)
-                        setTeamsLoaded(true)
-                        console.log(teamsArr);
                     } else {
                         handleSnackbar("Couldn't found the teams", "error");
                     }
 
                 })
                 .catch(err => {
-                    //console.error(err);
                     handleSnackbar("Couldn't found the teams", "error");
                 });
 
         }
 
 
-        //3. Fetch team squad data if user has selected a team
-        if (selectedTeamId !== undefined) {
-
-            var myHeaders = new Headers();
-            myHeaders.append("X-Auth-Token", "ef2c88344214470198023fffda588093");
-
-            var requestOptions = {
-                method: 'GET',
-                headers: myHeaders,
-                redirect: 'follow'
-            };
-
+        //2. Fetch team squad data if user has selected a team
+        if (!!selectedTeamId) {
 
             fetch(`http://api.football-data.org/v2/teams/${selectedTeamId}`, requestOptions)
                 .then(response => response.json())
@@ -255,7 +148,6 @@ function UserProfile(props) {
                             });
                         }
                         setSquadArr(squad)
-                        setSquadLoaded(true)
                     } else {
                         handleSnackbar("Couldn't found the squad", "error");
                     }
@@ -268,16 +160,13 @@ function UserProfile(props) {
         }
 
 
-        //END Fetch data from API
-
-    }, [selectedCompetitionId, selectedTeamId])
+    }, [selectedTeamId])
 
 
 
 
     const handleChange = e => {
         if (e.target.files[0]) {
-            setImage(e.target.files[0]);
             handleUpload(e.target.files[0]);
         }
     }
@@ -336,12 +225,9 @@ function UserProfile(props) {
         //Bind data to dropdowns
         props.firebase.user(uid).get().then(function (snapshot) {
             if (snapshot.exists()) {
-
-                setFav_competition(snapshot.val().fav_competition_name);
                 setFav_player(snapshot.val().fav_player_name);
                 setFav_team(snapshot.val().fav_team_name);
                 setUserName(snapshot.val().username);
-
             }
             else {
                 handleSnackbar("No data available", "error");
@@ -374,7 +260,6 @@ function UserProfile(props) {
                     />
                 </UserName>
 
-                <Dropdown placeholder={'Choose your favorite competition'} dataSet={competitionsArr} dropdownId="COMPETITION" favorite={fav_competition} onChange={eventhandler} />
                 <Dropdown placeholder={'Choose your favorite team'} dataSet={teamsArr} dropdownId="TEAMS" favorite={fav_team} onChange={eventhandler} />
                 <Dropdown placeholder={'Choose your favorite player'} dataSet={squadArr} dropdownId="PLAYERS" favorite={fav_player} />
 
@@ -382,8 +267,6 @@ function UserProfile(props) {
                     <SnackbarComponent severity={severity} message={message} clearSnackbar={clearSnackbar} /> : null
                 }
             </UserComp>
-
-            {/* <PlayerChart /> */}
 
         </Container>
 
