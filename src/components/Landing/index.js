04b48e11e0db310777df11ff8dc21c49ef9f6dd1
Ivar <ivarmun@gmail.com>
2021-04-20 17:03:43 +0200

    readme and landing fixed

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -26,50 +26,11 @@ const Landing = () => {
           <BgContent img={"https://wallpaperaccess.com/full/1150517.jpg"}>
             <Main>
               <Content>
-                <H1>Welcome to Football statistic</H1>
+                <H1>Welcome to Football Statistics</H1>
                 <P>View your favourite teams data and statistics for free</P>
                 <BgButtonI>
-                  <BgButton href="#info">Get Started</BgButton>
+                  <BgButton><Link to={ROUTES.SIGN_UP}>Get Started</Link></BgButton>
                 </BgButtonI>
-                <Infoboxes id="info">
-                  <BoxContainer>
-                    <Box>
-                      <h3>Statistics</h3>
-                      <p>
-                        View statistics of your favorite team and players,
-                        <br />
-                        with a wide range to choose from. Each user can select
-                        their own favorites.
-                      </p>
-                    </Box>
-                    <Box>
-                      <h3>Match Data</h3>
-                      <p>
-                        Track upcoming, finished, and ongoing matches. With
-                        Football Statistics own match-data feature.
-                      </p>
-                    </Box>
-                    <Box>
-                      <h3>Theme By Choice</h3>
-                      <p>
-                        Football Statistics lets the user select a theme to
-                        their liking. You can bind your own personal favorite
-                        theme to your account.
-                      </p>
-                    </Box>
-                  </BoxContainer>
-                </Infoboxes>
-
-                <TrySign>
-                  <h2>Like what you see?</h2>
-                  <p>
-                    Try out the application by signing up. Click the button
-                    below!
-                  </p>
-                  <TrySignButton>
-                    <Link to={ROUTES.SIGN_UP}>Sign Up!</Link>
-                  </TrySignButton>
-                </TrySign>
               </Content>
             </Main>
           </BgContent>
